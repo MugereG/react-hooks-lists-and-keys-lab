2452948939655623e4777e69fd8a1410ceb3e33c
Gideon Mugere <gideonnemugere@gmail.com>
2023-08-18 08:41:24 +0300

    Labs Assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,10 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */projects.map((project, index)=>(
+        <ProjectItem key={index} project={project} />
+      ))}
+      </div>
     </div>
   );
 }
